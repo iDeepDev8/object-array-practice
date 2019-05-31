a793bc3c5680d8152abf36e6224ac9b7733b263a
Anisha Dhana <anishadhana@gmail.com>
2019-06-01 10:29:24 +1200

    test suit is working

---
+++ tasks.test.js
@@ -0,0 +1,13 @@
+const album = require('./tasks') 
+
+test('Test suite is working', () => {
+  expect(true).toBeTruthy()
+})
+
+test('Displays names of all listeners', () => {
+
+})
+
+// ARRANGE
+// ACT
+// ASSERT
