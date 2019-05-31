e4b6f081838811177352cfdfd999a786b9a5d00e
noel <noelsoong@live.com>
2019-05-31 19:52:27 +1200

    :arrow_up: Init jest tests

---
+++ tests/tasks.test.js
@@ -0,0 +1,29 @@
+const Tasks = require('../tasks')
+
+test('should display the names of all the listeners', () => {
+    
+});
+
+test('should display the listeners and their genres ', () => {
+    
+});
+
+test('should display the name of the pop artists ', () => {
+    
+});
+
+test('should display the IDs of the rap and trap artists', () => {
+    
+});
+
+test('should display the artist names of Debra\'s genres', () => {
+    
+});
+
+test('should display the release names of Jono\'s genres', () => {
+    
+});
+
+test('should artists and release naems of all the listners', () => {
+    
+});
