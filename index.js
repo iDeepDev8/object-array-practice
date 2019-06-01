33dee9081b01b15a95d908475413cc594a470de1
noel <noelsoong@live.com>
2019-06-01 14:28:54 +1200

    :arrow_up: Display the names of all pop artists

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ console.log("")
 Tasks.getListenerGenres(listeners)
     .forEach(element => console.log(`${element.name}'s generes: ${element.genres}`));
 console.log("")
+
+// Display the name of the pop artists
+console.log('Pop Artists:')
+Tasks.getArtistNames(artists, ['pop'])
+    .forEach(artist => console.log(`    ${artist}`))
+console.log("")
