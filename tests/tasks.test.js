70bce757731ae0a4a5fff4efb21124a299052b20
noel <noelsoong@live.com>
2019-06-01 13:13:42 +1200

    :arrow_up: Refactor function names

--- tests/tasks.test.js
+++ tests/tasks.test.js
@@ -9,9 +9,9 @@ test('should display the names of all the listeners', () => {
     const expected = ['Debora', 'Jono', 'Stina']
     const listeners = [{ name: 'Debora' }, { name: 'Jono' }, { name: 'Stina' }]
     // Act
-    const actual = Tasks.displayListenerNames(listeners)
+    const actual = Tasks.getListenerNames(listeners)
     // Assert
-    expect(actual).toBe(expected)
+    expect(actual).toEqual(expected)
 });
 
 test('should display the listeners and their genres ', () => {
@@ -19,19 +19,19 @@ test('should display the listeners and their genres ', () => {
     const expected = [{ name: 'Debora', genres: ['trap', 'bop'] }, { name: 'Jono', genres: ['trance', 'techno'] }, { name: 'Stina', genres: ['jazz', 'rnb', 'mumble'] }]
     const listeners = [{ name: 'Debora', genres: ['trap', 'bop'], age: 23 }, { name: 'Jono', genres: ['trance', 'techno'], age: 25 }, { name: 'Stina', genres: ['jazz', 'rnb', 'mumble'], age: 19 }]
     // Act
-    const actual = Tasks.displayListenerGenres(listeners)
+    const actual = Tasks.getListenerGenres(listeners)
     // Assert
-    expect(actual).toBe(expected)
+    expect(actual).toEqual(expected)
 });
 
 test('should display the name of the pop artists ', () => {
     // Arrange
-    const expected = ['Rihana', 'Kanye', 'Knifeparty']
+    const expected = ['Rihana']
     const artists = [{ name: 'Rihana', genre: 'pop' }, { name: 'Kanye', genre: 'rap' }, { name: 'Knifeparty', genre: 'trap' }]
     // Act
     const actual = Tasks.getArtistNames(artists, ['pop'])
     // Assert
-    expect(actual).toBe(expected)
+    expect(actual).toEqual(expected)
 });
 
 test('should display the IDs of the rap and trap artists', () => {
