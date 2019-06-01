f3b6de80b7eff8a0b12cc09ed0127ad9dff494fa
noel <noelsoong@live.com>
2019-06-01 14:28:23 +1200

    :arrow_up: Display the names of all listeners

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const listeners = require('./data/listeners')
 const releases = require('./data/releases')
 const artists = require('./data/artists')
 const Tasks = require('./tasks')
+// Display the names of all of the listeners
+console.log("Listeners: ")
+Tasks.getListenerNames(listeners)
+    .forEach(name => console.log(`    ${name}`))
+console.log("")
