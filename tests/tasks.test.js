9f68855c83277f251b166a057c1555e0b724f4d0
noel <noelsoong@live.com>
2019-06-02 20:43:56 +1200

    :zap: Refactor Test for get artists and releases names

--- tests/tasks.test.js
+++ tests/tasks.test.js
@@ -56,24 +56,63 @@ test('should display the artist names of Listeners\'s genres', () => {
     expect(actual).toEqual(expected)
 });
 
-test('should display the names of the releases by Queen', () => {
+test('should display the names of the releases by Queen',  () => {
     // Arrange
-    const artist = 'Queen'
-    const artists = [{ id: 1, name: 'Queen', genre: 'rock' }, { id: 2, name: 'Armin', genre: 'trance' }, { id: 3, name: 'Knifeparty', genre: 'trap' }]
+    const artistId = 1;
     const releases = [{ id: 1, artistId: 1, name: 'Night at the Opera', genre: 'Rock', year: 1975 }, { id: 2, artistId: 2, name: 'Mirage', genre: 'Trance', year: 2010 }]
     const expected = ['Night at the Opera']
     // Act
-    const actual = Tasks.getArtistReleases(artist, artists, releases)
+    const actual = Tasks.getArtistReleases(artistId, releases)
     // Assert
     expect(actual).toEqual(expected)
 });
 
-// test('should display artists and release names of all the listners', () => {
-//     // Arrange
-//     const expected = ['Knifeparty']
-//     const genres = ['trap', 'bop']
-//     // Act
-//     const actual = Tasks.getArtistNames(artists, ['pop'])
-//     // Assert
-//     expect(actual).toBe(expected)
-// });
+test('should display artists and release names of all the listners', () => {
+    // Arrange
+    const expected = [{
+        name: 'Debora',
+        genres: ['trap', 'bop'],
+        age: 23,
+        artists: [{
+            id: 3,
+            name: 'Knifeparty',
+            genre: 'trap',
+            releases: ['Abandon Ship']
+        }]
+    },
+    {
+        name: 'Jono',
+        genres: ['trance', 'techno'],
+        age: 25,
+        artists: [{ id: 2, name: 'Armin', genre: 'trance', releases: ['Mirage'] }]
+    },
+    {
+        name: 'Stina',
+        genres: ['jazz', 'rnb', 'rock'],
+        age: 19,
+        artists: [{
+            id: 1,
+            name: 'Queen',
+            genre: 'rock',
+            releases: ['Night at the Opera']
+        }]
+    }]
+    const listeners = [{ name: 'Debora', genres: ['trap', 'bop'], age: 23 }, { name: 'Jono', genres: ['trance', 'techno'], age: 25 }, { name: 'Stina', genres: ['jazz', 'rnb', 'rock'], age: 19 }]
+    const artists = [{ id: 1, name: 'Queen', genre: 'rock' }, { id: 2, name: 'Armin', genre: 'trance' }, { id: 3, name: 'Knifeparty', genre: 'trap' }]
+    const releases = [{ id: 1, artistId: 1, name: 'Night at the Opera', genre: 'Rock', year: 1975 }, { id: 2, artistId: 2, name: 'Mirage', genre: 'Trance', year: 2010 }, { id: 3, artistId: 3, name: 'Abandon Ship', genre: 'Trap', year: 2010 }]
+
+    // Act
+    const actual = listeners
+        .map(listener => {
+            listener.artists = Tasks
+                .getArtistIDs(artists, listener.genres)
+                .map(artistId => {
+                    let artist = artists.find(artist => artist.id === artistId)
+                    artist.releases = Tasks.getArtistReleases(artist.id, releases)
+                    return artist
+                })
+            return listener
+        })
+    // Assert
+    expect(actual).toEqual(expected)
+});
