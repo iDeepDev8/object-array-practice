ad81d744a8e70b5d745f0d88615d28055ffb406f
noel <noelsoong@live.com>
2019-06-02 22:36:38 +1200

    :arrow_up: Refactor: Prettify big arrow functions

--- tasks.js
+++ tasks.js
@@ -1,49 +1,40 @@
 // Receive Array listners as param, returns Array of listener names
 function getListenerNames(listeners) {
-    return listeners.map(e => {
-        return e.name
-    })
+    return listeners.map(listener => listener.name)
 }
 
 // Receive Receive Array listners as param, returns Array of Objects
 // Objects have name and genres as properties
 function getListenerGenres(listeners) {
-    return listeners.map(e => {
-        return { name: e.name, genres: [...e.genres] }
+    return listeners.map(listener => {
+        return { name: listener.name, genres: [...listener.genres] }
     })
 }
 
 // Receive Array artists, genres as params, returns Array of artist names
 function getArtistNames(artists, genres) {
     return artists
-        .filter(artist => {
-            return genres.includes(artist.genre)
-        })
-        .map(artist => {
-            return artist.name
-        })
+        .filter(artist => genres.includes(artist.genre))
+        .map(artist => artist.name)
 }
 
 // Receive Array artists, genres as params, returns Array of artist IDs
 function getArtistIDs(artists, genres) {
     return artists
-        .filter(artist => {
-            return genres.includes(artist.genre)
-        })
-        .map(artist => {
-            return artist.id
-        })
+        .filter(artist => genres.includes(artist.genre))
+        .map(artist => artist.id)
 }
 
 // Receive artistId, Array releases as params, returns Array of artist's releases
 function getArtistReleases(artistId, releases) {
     return releases
-    .filter( release => release.artistId === artistId)
-    .map(release => release.name)
+        .filter(release => release.artistId === artistId)
+        .map(release => release.name)
 }
 
 // Receive name, Array artists as params, returns Object Artist
-    return artists.find(artist=> artist.name === name)
+function getArtist(name, artists) {
+    return artists.find(artist => artist.name === name)
 }
 
 // Exports
