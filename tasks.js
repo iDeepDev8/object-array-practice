a13c86623482ea6a9a81aedf0c134661832093c0
noel <noelsoong@live.com>
2019-06-02 22:36:00 +1200

    :arrow_up: Refactor: Add comments

--- tasks.js
+++ tasks.js
@@ -1,15 +1,19 @@
+// Receive Array listners as param, returns Array of listener names
 function getListenerNames(listeners) {
     return listeners.map(e => {
         return e.name
     })
 }
 
+// Receive Receive Array listners as param, returns Array of Objects
+// Objects have name and genres as properties
 function getListenerGenres(listeners) {
     return listeners.map(e => {
         return { name: e.name, genres: [...e.genres] }
     })
 }
 
+// Receive Array artists, genres as params, returns Array of artist names
 function getArtistNames(artists, genres) {
     return artists
         .filter(artist => {
@@ -20,6 +24,7 @@ function getArtistNames(artists, genres) {
         })
 }
 
+// Receive Array artists, genres as params, returns Array of artist IDs
 function getArtistIDs(artists, genres) {
     return artists
         .filter(artist => {
@@ -30,16 +35,18 @@ function getArtistIDs(artists, genres) {
         })
 }
 
+// Receive artistId, Array releases as params, returns Array of artist's releases
 function getArtistReleases(artistId, releases) {
     return releases
     .filter( release => release.artistId === artistId)
     .map(release => release.name)
 }
 
-function getArtist(name, artists){
+// Receive name, Array artists as params, returns Object Artist
     return artists.find(artist=> artist.name === name)
 }
 
+// Exports
 module.exports = {
     getListenerNames,
     getListenerGenres,
