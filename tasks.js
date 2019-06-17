5312a0b783caae9495e1846166db57943ef62a8b
Keith <uniyoona@naver.com>
2019-06-17 23:13:40 +1200

    started doing object-arrays practice

---
+++ tasks.js
@@ -0,0 +1,11 @@
+// Display the names of all of the listeners
+
+const listeners = require('./data/listeners')
+
+function displayListeners() {
+    const arrayListener = []
+    const listen = arrayListener.push(listeners)
+    return listen
+}
+
+module.exports = displayListeners
---
+++ tasks.test.js
@@ -0,0 +1,10 @@
+const displayListeners = require('./tasks')
+
+test('displayListeners display listeners from listeners.js file', () => {
+    // Arrange
+    const arrayListener = []
+    const listen = arrayListener.push(listeners)
+    // Act
+    const actual = 
+    // Assert
+})
